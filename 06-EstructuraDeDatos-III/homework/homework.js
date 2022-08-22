6e56efeb1e72c1469eac6680a13f03fc743a2f4f
riverofrancisco <francisco.rivero124@gmail.com>
2022-08-22 19:22:04 -0300

    Data Structures III - 2 functions left

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,7 +11,87 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(data) {
+      this.value = data;
+      this.left = null;
+	    this.right = null;
+}
+
+BinarySearchTree.prototype.size = function(){
+  var size = 1;
+  if(this.left){
+    size+= 1;
+    this.left.size();
+  }
+  if(this.right){
+    size+= 1;
+    this.right.size();
+  }
+  return size;
+}
+
+BinarySearchTree.prototype.insert = function(valor){  
+
+    if(valor < this.value){
+      if(this.left === null){
+        this.left = new BinarySearchTree(valor);
+      }else{
+        this.left.insert(valor);
+      }	
+    }
+    if( valor > this.value){
+      if(this.right === null){
+        this.right = new BinarySearchTree(valor);
+      }else{
+        this.right.insert(valor);
+      }	
+    }
+
+};
+
+BinarySearchTree.prototype.contains = function(buscado){
+if (buscado === this.value) return true;//ok
+//todos los casos
+
+if(buscado > this.value){
+  if(this.right == null)return false;
+  else {return this.right.contains(buscado);}
+}
+if (buscado < this.value){
+  if(this.left == null) return false;
+  else {return this.left.contains(buscado);}
+}
+
+/* if (buscado < this.value && this.left !== null){
+  if (buscado === this.left.value) {return true;}
+  else {this.left.contains(buscado);}
+  }
+
+if (buscado > this.value && this.right !== null){
+  if (buscado === this.right.value) {return true;}
+  else {this.right.contains(buscado);}
+  }
+return false;
+ */
+//codigo jona
+};
+
+BinarySearchTree.prototype.depthFirstForEach = function(orden){
+  if (orden === 'pre-order') {
+    while (this.left){
+    }
+  }
+  if (orden === 'post-order') {}
+  if (orden === 'in-order' || orden === null) {}
+};
+
+/* In-order Traversal (izquierda-raíz-derecha)
+Pre-order transversal (raíz-izquierda-derecha)
+Recorrido posterior al pedido (raíz izquierda-derecha) */
+
+
+BinarySearchTree.prototype.breadthFirstForEach = function(){};
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
