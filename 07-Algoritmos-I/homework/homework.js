d99c2f5d9c20c96d692bfac280e68bc878e31f72
riverofrancisco <francisco.rivero124@gmail.com>
2022-08-23 22:36:13 -0300

    Algoritmos I - 2 tests passed

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,6 +6,13 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+let factores = [];
+while(num % factor === 0){
+
+  factores.push(factor)
+  num = num/2;}
+
+  return arr.sort();
 
 }
 
@@ -15,14 +22,26 @@ function bubbleSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
-}
-
+  function iteracion(){
+    for (let i = 0; i < array.length; i++){
+      if (array[i] > array[i + 1]){
+          let menor = array[i + 1];
+          let mayor = array[i];
+          array[i + 1] = mayor;
+          array[i] = menor;
+          iteracion(array);}
+      }
+      }
+        iteracion()
+        return array
+      }
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
 
 }
 
@@ -32,9 +51,21 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var ordenado = [];
+  function pusheo(){
+      let min = array[0]
+      while(array.length > 0){
+          for (let i = 0; i < array.length ; i++){
+                  if(min > array[i]){
+                      min = array[i];
+                      var pos = i}}
+          ordenado.push(min);
+          array.splice(pos,1);
+          pusheo(array);
+  }}
+  pusheo();
+  return ordenado;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
